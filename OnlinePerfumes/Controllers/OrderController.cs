96c5b1e30caf976875a5ce4def6839ce1df5f51f
AleksNikiforova <aleksandranikiforova@abv.bg>
2025-02-02 14:28:32 +0200

    Change OrderContoller and make index and add view for order

--- OnlinePerfumes/Controllers/OrderController.cs
+++ OnlinePerfumes/Controllers/OrderController.cs
@@ -16,19 +16,29 @@ namespace OnlinePerfumes.Controllers
             _orderservice = orderservice;
             _productservice = productservice;
         }
-        public async Task<IActionResult> Add()
+        /*public async Task<IActionResult> Add()
         {
             var products = await _productservice.GetAll();
             ViewBag.Products = new SelectList(products, "Id", "Name");
             return View();
 
-        }
+        }*/
         [HttpPost]
-        public async Task<IActionResult> Add(Order order)
+        public async Task<IActionResult> Add(Order order, int[]selectedProducts)
         {
+            var products = await _productservice.GetAll();
+            ViewBag.Products = new SelectList(products, "Id", "Name");
+            return View(order);
+            order.OrderProducts = selectedProducts.Select(productId => new OrderProduct
+            {
+                ProductId = productId
+            }).ToList();
+
             await _orderservice.Add(order);
             return RedirectToAction("Index");
-        }     
+        }
+
+   
         public async Task<IActionResult> Update(int id)
         {
             var order = await _orderservice.GetById(id);
